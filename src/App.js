5f4ba2d07866680e002274541066ddf3f86bdc84
CahTarine <camille.tarine@gmail.com>
2025-02-01 19:44:09 -0300

    criando componentes react

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import Header from './Header/Header';
+import Sidebar from './Sidebar/Sidebar';
+import Main from './Main/Main';
+import Footer from './Footer/Footer';
+import Script from './Script/script'
+
+function App() {
+  return (
+    <>
+      <Header></Header>
+      <Sidebar></Sidebar>
+      <Main></Main>
+      <Footer></Footer>
+      <Script></Script>
+    </>
+  )
+};
+
+export default App;
---
+++ src/Footer/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+import './Footer.css';
+
+const Footer = () => {
+    return (   
+    <footer class="premium">
+        <div class="text">
+            <p class="premium-text__title">EXPERIMENTE O PREMIUM DE GRAÇA</p>
+            <p class="premium-text__subtitle">Inscreva-se para curtir música ilimitada e podcasts só com alguns anúncios. Não precisa de cartão de crédito.</p>
+        </div>
+        <div class="button">
+            <button type="button">Inscreva-se grátis</button>
+        </div>
+    </footer>
+    )
+}
+
+export default Footer;
